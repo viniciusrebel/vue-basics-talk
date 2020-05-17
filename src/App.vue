e0ff3e2a8c20231246bace3c7fba1f9be31486ae
Vinicius Rebel <vinicius.rebel@gmail.com>
2020-05-17 12:55:57 -0400

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="app-header">
+      <img 
+        alt="Vue logo"
+        src="./assets/logo.png"
+      >
+      <h1>Vue Basics</h1>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.app-header{
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+  border-bottom: 1px solid #dbdbdb;
+}
+.app-header img {
+    height: 30px;
+    margin-right: 16px;
+  }
+.app-header h1 {
+  margin: 0;
 }
 </style>
